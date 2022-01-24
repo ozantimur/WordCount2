ccdbe6e644a09af675e400db5261038cd571a8dd
Mehmet Akşahin <mehmet.aksahin@testinium.com>
2022-01-24 20:43:49 +0300

    lkdfsdjkfhskhdfkjsdf

--- src/main/java/com/timurholding/Main.java
+++ src/main/java/com/timurholding/Main.java
@@ -6,7 +6,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("hello world");
+        System.out.println("hello world"); // sdndljsjkfhsjkhfksdj
 
         Scanner in = new Scanner(System.in);
 
